5c7b4701965652d20f6370a12223016af34983ba
arpan-jk <arpan.koley@jktech.com>
2025-06-13 15:02:51 +0000

    reconfigured folder structure

--- app/models/schema.py
+++ app/models/schema.py
@@ -1,9 +1,7 @@
 from pydantic import BaseModel
-from typing import List
 
 class User(BaseModel):
     id:int
     name:str
     address:str
 
-users:List[User]=[]
---
+++ app/utils/db.py
@@ -0,0 +1,5 @@
+
+from typing import List
+from app.models.schema import User
+
+users:List[User]=[]
