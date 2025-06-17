d2b0c87ca3cce5499a0916a3e923f7202fe86d10
AmanRezaa <aman.reza@jktech.com>
2025-06-17 06:14:34 +0000

    changes saved

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from routes import create_user, get_user, update_user, delete_user
 
 app=FastAPI()
 
+
 app.include_router(create_user.router)
 app.include_router(get_user.router)
 app.include_router(update_user.router)
