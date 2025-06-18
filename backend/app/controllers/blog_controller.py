4d9b8a17b50bff423708895d9bdfee422399b37a
AmanRezaa <aman.reza@jktech.com>
2025-06-18 07:18:20 +0000

    blogs routes error handled

--- backend/app/controllers/blog_controller.py
+++ backend/app/controllers/blog_controller.py
@@ -1,83 +1,98 @@
-# from app.models.schema import Blog
-# from app.utils.db import blogs
-
-# def get_blogs():
-#     return blogs
-
-# def create_blog(blog:Blog):
-#     blogs.append(blog)
-#     return {"message":"New blog added successfully"}
-
-# def get_blog(blog_id:int):
-#     for index, blog in enumerate(blogs):
-#         if blog.id==blog_id:
-#             return blogs[index]
-#     return {"error":"the id not found. There was an error while updating the data"}
-
-# def update_blog(blog_id:int, updated_blog:Blog):
-#     for index, blog in enumerate(blogs):
-#         if blog.id==blog_id:
-#             blogs[index]=updated_blog
-#             return updated_blog
-#     return {"error":"the id not found. There was an error while updating the data"}
-
-
-# def delete_blog(blog_id:int):
-#     for index, blog in enumerate(blogs):
-#         if blog.id==blog_id:
-#             deleted_blog=blogs.pop(index)
-#             return deleted_blog
-#     return {"error":"tea_id not found. There was an error while deleting the data"}
-
-
-
-
-# app/controllers/blog_controller.py
+from fastapi import HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
-from sqlalchemy.exc import NoResultFound
+from sqlalchemy.exc import SQLAlchemyError
 from app.models.schema import Blog
 from app.models.pydantic_models import BlogCreate
 
+
 async def create_blog(blog: BlogCreate, db: AsyncSession):
-    new_blog = Blog(**blog.dict())
-    db.add(new_blog)
-    await db.commit()
-    await db.refresh(new_blog)
-    return new_blog
+    try:
+        new_blog = Blog(**blog.dict())
+        db.add(new_blog)
+        await db.commit()
+        await db.refresh(new_blog)
+        return new_blog
+    except SQLAlchemyError as e:
+        await db.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Error while creating blog"
+        )
+
 
 async def get_all_blogs(db: AsyncSession):
-    result = await db.execute(select(Blog))
-    return result.scalars().all()
+    try:
+        result = await db.execute(select(Blog))
+        blogs = result.scalars().all()
+        return blogs
+    except SQLAlchemyError:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Error while fetching blogs"
+        )
+
 
 async def get_blog(blog_id: int, db: AsyncSession):
-    result = await db.execute(select(Blog).where(Blog.id == blog_id))
-    blog = result.scalar_one_or_none()
-    return blog
+    try:
+        result = await db.execute(select(Blog).where(Blog.id == blog_id))
+        blog = result.scalar_one_or_none()
+        if blog is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"Blog with ID {blog_id} not found"
+            )
+        return blog
+    except SQLAlchemyError:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Error while fetching the blog"
+        )
+
 
 async def update_blog(blog_id: int, updated_blog: BlogCreate, db: AsyncSession):
-    result = await db.execute(select(Blog).where(Blog.id == blog_id))
-    blog = result.scalar_one_or_none()
-    if not blog:
-        return None
+    try:
+        result = await db.execute(select(Blog).where(Blog.id == blog_id))
+        blog = result.scalar_one_or_none()
 
-    for key, value in updated_blog.dict().items():
-        setattr(blog, key, value)
+        if blog is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"Blog with ID {blog_id} not found"
+            )
 
-    db.add(blog)
-    await db.commit()
-    await db.refresh(blog)
-    return blog
+        for key, value in updated_blog.dict().items():
+            setattr(blog, key, value)
+
+        db.add(blog)
+        await db.commit()
+        await db.refresh(blog)
+        return blog
+    except SQLAlchemyError:
+        await db.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Error while updating the blog"
+        )
+
 
 async def delete_blog(blog_id: int, db: AsyncSession):
-    result = await db.execute(select(Blog).where(Blog.id == blog_id))
-    blog = result.scalar_one_or_none()
-    if not blog:
-        return None
+    try:
+        result = await db.execute(select(Blog).where(Blog.id == blog_id))
+        blog = result.scalar_one_or_none()
 
-    await db.delete(blog)
-    await db.commit()
-    return True
+        if blog is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"Blog with ID {blog_id} not found"
+            )
 
-
-
+        await db.delete(blog)
+        await db.commit()
+        return {"message": f"Blog with ID {blog_id} deleted successfully"}
+    except SQLAlchemyError:
+        await db.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Error while deleting the blog"
+        )
